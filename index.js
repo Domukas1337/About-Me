8b26e501e578a6d359c5bc88a381d6196fc76b22
Domukas1337 <domukas1337@gmail.com>
2023-12-23 19:32:26 +0200

    add comments

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 function switchContent(index){
+    // get content and button
     var contentSection = document.getElementsByClassName("content");
     var buttonSection = document.getElementsByClassName("buttons");
+
+    // remove active class from all contents
     for (var i = 0; i < contentSection.length; i++){
         contentSection[i].classList.remove("active");
         buttonSection[i].classList.remove("active");
     }
+    // get selected content and button
     var selectedContent = document.getElementById("content" + index);
     var selectedButton = document.getElementById("button" + index);
+    
+    // add active class to selected content
     selectedContent.classList.add("active");
     selectedButton.classList.add("active");
 }
