3624748c89f5a7b1c99aa16bf41fca6c6fb7f70a
Domukas1337 <domukas1337@gmail.com>
2023-12-23 19:28:28 +0200

    css cool

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function switchContent(index){
     var contentSection = document.getElementsByClassName("content");
+    var buttonSection = document.getElementsByClassName("buttons");
     for (var i = 0; i < contentSection.length; i++){
         contentSection[i].classList.remove("active");
+        buttonSection[i].classList.remove("active");
     }
     var selectedContent = document.getElementById("content" + index);
+    var selectedButton = document.getElementById("button" + index);
     selectedContent.classList.add("active");
+    selectedButton.classList.add("active");
 }
